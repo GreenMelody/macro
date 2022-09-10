c6a768a0f6528bbefbde4b1ae29b184b956d7608
Minsu Kim <cafri2580@gmail.com>
2022-09-11 01:26:52 +0900

    add actDeactWidgets function

--- macro_for_thankyou.py
+++ macro_for_thankyou.py
@@ -14,29 +14,21 @@ window.resizable(True,True)
 font_15 = font.Font(size=15)
 font_15_bold = font.Font(size=15, weight='bold')
 
-keys_arr = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
-')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
-'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
-'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
-'browserback', 'browserfavorites', 'browserforward', 'browserhome',
-'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
-'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
-'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
-'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
-'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
-'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
-'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
-'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
-'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
-'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
-'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
-'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
-'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
-'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
-'command', 'option', 'optionleft', 'optionright']
-target_point_arr=[]     #save target point absolute screen point
+select_func_list = ['Mouse', 'Keyboard', 'Hotkey', 'Write' 'Text']
+mouse_func_list = ['L-Click', 'L-Down', 'L-Up', 'R-Click', 'R-Down', 'R-Up', 'Double', 'Move', 'Drag']
+key_func_list = ['Press', 'KeyDown', 'KeyUp']
+key_list = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7','8', '9',
+            'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12',
+            'space', 'backspace', 'tab', 'alt', 'altleft', 'altright', 'ctrl', 'ctrlleft', 'ctrlright', 'shift', 'shiftleft', 'shiftright', 'del', 'delete', 'left', 'right', 'up', 'down', 'end', 'enter', 'esc', 'escape', 'hanguel', 'hangul', 'home', 'insert',
+            'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn', 'pgup', 'printscreen', 'prntscrn', 'scrolllock',
+            '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',  ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+            
+
+hotkey_list = ['Ctrl+A', 'Ctrl+C', 'Ctrl+V']
+
+
+save_arr=[]             #save all things, [act/deact, num, select_func, mouse_func, mouse_X, mouseY, key_func, key, hotkey, text, delay]
 
 t_cnt=0                 #total target window number
 target_win_arr=[]       #save target windows
@@ -64,36 +56,41 @@ def makeTarget():
     target_win_arr[t_cnt].overrideredirect(True)
 
     #show target num and target point '+'
-    
     target_lb1_arr.append(tk.Label(target_win_arr[t_cnt], borderwidth=1, relief='ridge', bg='red', text='1111'))
     target_lb2_arr.append(tk.Label(target_win_arr[t_cnt], borderwidth=1, relief='ridge', width=3, font=font_15, fg='red', text='+'))
     target_lb1_arr[t_cnt].grid(row=0, column=0,sticky='ns')
     target_lb2_arr[t_cnt].grid(row=0, column=1)    
 
+    #create checkbutton, label, combobox, entry
     chk_var.append(tk.IntVar())
     act_chk_btn_arr         .append(ttk.Checkbutton(set_target_lbframe, variable=chk_var[t_cnt]))
     no_lb_arr               .append(tk.Label(set_target_lbframe, text=str(t_cnt+1)))
-    select_func_combo_arr .append(ttk.Combobox(set_target_lbframe))
-    mouse_func_combo_arr  .append(ttk.Combobox(set_target_lbframe))
-    key_func_combo_arr    .append(ttk.Combobox(set_target_lbframe))
-    key_combo_arr         .append(ttk.Combobox(set_target_lbframe))
-    hotkey_combo_arr      .append(ttk.Combobox(set_target_lbframe))
+    select_func_combo_arr .append(ttk.Combobox(set_target_lbframe, state='readonly', width=8, values=select_func_list))
+    mouse_func_combo_arr  .append(ttk.Combobox(set_target_lbframe, state='readonly', width=8, values=mouse_func_list))
+    key_func_combo_arr    .append(ttk.Combobox(set_target_lbframe, state='readonly', width=8, values=key_func_list))
+    key_combo_arr         .append(ttk.Combobox(set_target_lbframe, state='readonly', width=8, values=key_list))
+    hotkey_combo_arr      .append(ttk.Combobox(set_target_lbframe, state='readonly', width=8, values=hotkey_list))
     text_entry_arr          .append(tk.Entry(set_target_lbframe))
-    delay_entry_arr         .append(tk.Entry(set_target_lbframe))
+    delay_entry_arr         .append(tk.Entry(set_target_lbframe, justify='right', width=8))
 
+    #grid checkbutton, label, combobox, entry
     act_chk_btn_arr[t_cnt]          .grid(row=t_cnt+2, column=0, padx=(5,5), pady=(5,0))
-    chk_var[t_cnt].set(1)
     no_lb_arr[t_cnt]                .grid(row=t_cnt+2, column=2, padx=(5,5), pady=(5,0))
-    select_func_combo_arr[t_cnt]  .grid(row=t_cnt+2, column=4, padx=(5,5), pady=(5,0))
-    mouse_func_combo_arr[t_cnt]   .grid(row=t_cnt+2, column=6, padx=(5,5), pady=(5,0))
-    key_func_combo_arr[t_cnt]     .grid(row=t_cnt+2, column=8, padx=(5,5), pady=(5,0))
-    key_combo_arr[t_cnt]          .grid(row=t_cnt+2, column=10, padx=(5,5), pady=(5,0))
-    hotkey_combo_arr[t_cnt]       .grid(row=t_cnt+2, column=12, padx=(5,5), pady=(5,0))
+    select_func_combo_arr[t_cnt]    .grid(row=t_cnt+2, column=4, padx=(5,5), pady=(5,0))
+    mouse_func_combo_arr[t_cnt]     .grid(row=t_cnt+2, column=6, padx=(5,5), pady=(5,0))
+    key_func_combo_arr[t_cnt]       .grid(row=t_cnt+2, column=8, padx=(5,5), pady=(5,0))
+    key_combo_arr[t_cnt]            .grid(row=t_cnt+2, column=10, padx=(5,5), pady=(5,0))
+    hotkey_combo_arr[t_cnt]         .grid(row=t_cnt+2, column=12, padx=(5,5), pady=(5,0))
     text_entry_arr[t_cnt]           .grid(row=t_cnt+2, column=14, padx=(5,5), pady=(5,0))
     delay_entry_arr[t_cnt]          .grid(row=t_cnt+2, column=16, padx=(5,5), pady=(5,0))
 
-
-
+    chk_var[t_cnt].set(1)                       #always checked when it created
+    select_func_combo_arr[t_cnt].current(0)     #select first value
+    mouse_func_combo_arr[t_cnt].current(0)     #select first value
+    key_func_combo_arr[t_cnt].current(0)     #select first value
+    key_combo_arr[t_cnt].current(0)     #select first value
+    hotkey_combo_arr[t_cnt].current(0)     #select first value
+    actDeactWidgets('')
     #to move target point using mouse drag
     def drag(event):
         width = str(target_win_arr[t_cnt].winfo_width())
@@ -104,6 +101,10 @@ def makeTarget():
 
     #mouse left drag event
     target_win_arr[t_cnt].bind("<B1-Motion>", drag)
+    #only digit input
+    delay_entry_arr[t_cnt].bind('<KeyRelease>', onlyNumbers)
+    #when combobox selected, widget condition will be change 
+    select_func_combo_arr[t_cnt].bind('<<ComboboxSelected>>', actDeactWidgets)
 
 
 #delete target and list
@@ -140,9 +141,47 @@ def delTarget():
     else:
         print('no widget to delete')
 
-
+#when you push the start button, it will save target points and ect. only act checked
 def getTargetPoint():
+    t_cnt = len(target_win_arr)
+    for idx in range(0,t_cnt):
+        if(select_func_combo_arr[idx].get() == 'Mouse'):
+            pass
+  
+#disable comboboxes not necessary 
+def actDeactWidgets(event):
+    t_cnt = len(target_win_arr)
+    for idx in range(0, t_cnt):
+        if(select_func_combo_arr[idx].get() == 'Mouse'):
+            mouse_func_combo_arr[idx].config(state='readonly')
+            key_func_combo_arr[idx].config(state='disabled')
+            key_combo_arr[idx].config(state='disabled')
+            hotkey_combo_arr[idx].config(state='disabled')
+            text_entry_arr[idx].config(state='readonly')
+        elif(select_func_combo_arr[idx].get() == 'Keyboard'):
+            mouse_func_combo_arr[idx].config(state='disabled')
+            key_func_combo_arr[idx].config(state='readonly')
+            key_combo_arr[idx].config(state='readonly')
+            hotkey_combo_arr[idx].config(state='disabled')
+            text_entry_arr[idx].config(state='readonly')
+        elif(select_func_combo_arr[idx].get() == 'Hotkey'):
+            mouse_func_combo_arr[idx].config(state='disabled')
+            key_func_combo_arr[idx].config(state='disabled')
+            key_combo_arr[idx].config(state='disabled')
+            hotkey_combo_arr[idx].config(state='readonly')
+            text_entry_arr[idx].config(state='readonly')
+        elif(select_func_combo_arr[idx].get() == 'WriteText'):
+            mouse_func_combo_arr[idx].config(state='disabled')
+            key_func_combo_arr[idx].config(state='disabled')
+            key_combo_arr[idx].config(state='disabled')
+            hotkey_combo_arr[idx].config(state='disabled')
+            text_entry_arr[idx].config(state='normal')
+        
+       
+
+def allWidgetsActDeact():
     pass
+
 
 def moveToTargetPoint():
     pass
@@ -159,7 +198,7 @@ def moveToTargetPoint():
 ####set_init
 set_init_lbframe = tk.LabelFrame(window, text='Set Initial values')
 init_delay_lb = tk.Label(set_init_lbframe, text='Init delay(ms) : ')
-init_delay_entry = tk.Entry(set_init_lbframe, justify='right')
+init_delay_entry = tk.Entry(set_init_lbframe, justify='right', width=8)
 
 ####set_init GRID
 set_init_lbframe    .grid(row=0, column=0, padx=(5,0), pady=(5,0), sticky='news')
@@ -169,18 +208,18 @@ init_delay_entry    .grid(row=0, column=1, padx=(5,0), pady=(5,0))
 
 ####set_target_lbframe
 set_target_lbframe = tk.LabelFrame(window, text='Set Targets')
-#Act(chk_btn), No. , Select Func.(Mouse, Keyboard, Write Text, Hotkey), Mouse Func.(L-click, L-Down, L-Up, R-click, R-Down, R-Up, Double, Move, Drag), key Func.(Press, KeyDown, KeyUp), Key('a', 'b',...), HotKey(Ctrl+A, Ctrl+C, Ctrl+V), Text, Delay
+#Act(chk_btn), No. , Select Func.(Mouse, Keyboard, Hotkey, Write Text), Mouse Func.(L-click, L-Down, L-Up, R-click, R-Down, R-Up, Double, Move, Drag), key Func.(Press, KeyDown, KeyUp), Key('a', 'b',...), Hotkey(Ctrl+A, Ctrl+C, Ctrl+V), Text, Delay
 #it will execute just what you Act checked
 #when you select Mouse, then M-Function menu will be activated
 act_lb          = tk.Label(set_target_lbframe, anchor='center', text='Act')
 no_lb           = tk.Label(set_target_lbframe, anchor='center', text='No.')
-select_func_lb  = tk.Label(set_target_lbframe, anchor='center', text='Select Func.')
-mouse_func_lb   = tk.Label(set_target_lbframe, anchor='center', text='Mouse Func.')
-key_func_lb     = tk.Label(set_target_lbframe, anchor='center', text='Key Func')
-key_lb          = tk.Label(set_target_lbframe, anchor='center', text='Key')
-hotkey_lb       = tk.Label(set_target_lbframe, anchor='center', text='Hotkey')
-text_lb         = tk.Label(set_target_lbframe, anchor='center', text='Text')
-delay_lb        = tk.Label(set_target_lbframe, anchor='center', text='Delay(ms)')
+select_func_lb  = tk.Label(set_target_lbframe, anchor='center', width=12, text='Select Func.')
+mouse_func_lb   = tk.Label(set_target_lbframe, anchor='center', width=12, text='Mouse Func.')
+key_func_lb     = tk.Label(set_target_lbframe, anchor='center', width=12, text='Key Func')
+key_lb          = tk.Label(set_target_lbframe, anchor='center', width=12, text='Key')
+hotkey_lb       = tk.Label(set_target_lbframe, anchor='center', width=12, text='Hotkey')
+text_lb         = tk.Label(set_target_lbframe, anchor='center', width=20, text='Text')
+delay_lb        = tk.Label(set_target_lbframe, anchor='center', width=8, text='Delay(ms)')
 #seperators
 sep_v1          = ttk.Separator(set_target_lbframe, orient="vertical")
 sep_v2          = ttk.Separator(set_target_lbframe, orient="vertical")
@@ -209,9 +248,9 @@ sep_v6              .grid(row=0, column=11, sticky='ns', rowspan=999)
 hotkey_lb           .grid(row=0, column=12, padx=(5,5), pady=(5,0))
 sep_v7              .grid(row=0, column=13, sticky='ns', rowspan=999)
 text_lb             .grid(row=0, column=14, padx=(5,5), pady=(5,0))
-sep_v7              .grid(row=0, column=15, sticky='ns', rowspan=999)
+sep_v8              .grid(row=0, column=15, sticky='ns', rowspan=999)
 delay_lb             .grid(row=0, column=16, padx=(5,5), pady=(5,0))
-sep_h1              .grid(row=1, column=0, sticky='ew', columnspan=15) 
+sep_h1              .grid(row=1, column=0, sticky='ew', columnspan=17) 
 
 
 
@@ -252,6 +291,8 @@ def onlyNumbers(event):
         print(event.char)
     else:
         txt = str(init_delay_entry.get())
+        #init_delay_entry.delete(0,'end')
+        #init_delay_entry.insert(0, txt[:-1])
         init_delay_entry.delete(0,'end')
         init_delay_entry.insert(0, txt[:-1])
 
